29bcf4e1bfdcab7666d51fa59a8a35a648c1ff3f
noellekimiko <noelle.fakaji@yahoo.com>
2017-05-17 11:04:30 -0700

    EE3 Updates: Added addon.setup.php file, changed folder structure
    Changes to make the extension work with EE3: adding the new, required addon.setup.php file and renaming the folders to reflect the EE3 structure. None of the code in the extension has been changed.
    
    General Addon changes in EE3: https://docs.expressionengine.com/latest/development/conversion/index.html
    Details about the addon.setup.php file: https://docs.expressionengine.com/latest/development/addon_setup_php_file.html
    Changes needed for extensions in particular: https://docs.expressionengine.com/latest/development/conversion/extensions.html

---
+++ system/user/addons/ce_cache_breaker_for_low_variables/addon.setup.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	'author'			=> 'Matt Fordham',
+	'author_url'	=> 'http://www.revolvercreative.com',
+	'name'				=> 'CE Cache Breaker for Low Variables',
+	'description'	=> 'Allows for cache breaking in CE Cache when Low Variables are updated',
+	'version'			=> '1.1',
+	'namespace'		=> 'Ce_cache_breaker_for_low_variables',
+	'settings_exist'	=> TRUE,
+	'docs_url'		=> 'https://github.com/mattfordham/CE-Cache-Breaker-for-Low-Variables'
+);
